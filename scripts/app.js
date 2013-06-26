b0120301333ba815a578bda2805c2d54f9592fc1
hybridsteve <stephen.wagner@hybrid-learning.com>
2013-06-25 20:05:29 -0400

    Added some question logic. Questions are added to app question collection.

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,15 @@
 $(function() {
+	// initialize vars
 	App.currentUser = new App.Models.User();
+	var questions = new App.Collections.Questions();
+
+	// initialize views
 	var loginView = new App.Views.Login({
 		el: $("#loginView"),
 		model: App.currentUser
 	});
 	var newQuestionView = new App.Views.NewQuestion({
-		el: $("#newQuestionView")
+		el: $("#newQuestionView"),
+		model: questions
 	});
 });
--- scripts/views/new_question.js
+++ scripts/views/new_question.js
@@ -3,11 +3,23 @@ App.Views.NewQuestion = Backbone.View.extend({
 		App.currentUser.on("change", this.render, this);
 		this.render();
 	},
+	events: {
+		"click #addItem": "addItem"
+	},
 	render: function() {
 		// set asker name
 		this.$("#asker").text(App.currentUser.get("userName"));
 		// show/hide view
 		this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
 		return this;
+	},
+	addItem: function() {
+		var question = {
+			text: this.$("#newQuestion").val(),
+			userName: App.currentUser.get("userName")
+		};
+
+		this.model.add(new App.Models.Question(question));
+		this.$("#newQuestion").val("");
 	}
 });
